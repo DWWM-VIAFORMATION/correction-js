22739bb2a0e2064a61d40f0f333b3eefa41eb9e6
MedicArwen <thierry-bru@laposte.net>
2022-12-07 15:12:50 +0100

    correction série 1

---
+++ s1/serie-01/exo-01.js
@@ -0,0 +1,4 @@
+let prenom = "Thierry";
+let nom = "BRU"
+let age = 45;
+console.log(`je m'appelle ${prenom} ${nom} et j'ai ${age+1} ans l'année prochaine.`); 
---
+++ s1/serie-01/exo-05.js
@@ -0,0 +1,3 @@
+let rayon = 10;
+let surface = Math.pow(rayon,2)*Math.PI;
+console.log(`Le cercle de rayon ${rayon}cm a pour surface ${surface.toFixed(2)}cm².`);
