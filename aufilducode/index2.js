7ef953320f9724959aeb86fe0a4afb0a9108aab9
MedicArwen <thierry-bru@laposte.net>
2022-12-08 13:59:54 +0100

    correction serie 2 seance 1 js

---
+++ aufilducode/index2.js
@@ -0,0 +1,21 @@
+for(i=0;i<5000000;i++)
+{
+       //  console.log(i);
+         }
+         console.log(i);
+// for(i=0;i<5;++i)
+// {
+//     console.log(i);
+// }
+// let i =0;
+// while (i<5) {
+//    prompt('en avez vous assez?')
+// }
+// console.log("fini!");
+// let rep;
+// do
+// {
+//     console.log("je fais le taff une fois");
+//      rep = prompt("Tapez y pour refaire")
+// }
+// while (rep == "y")
