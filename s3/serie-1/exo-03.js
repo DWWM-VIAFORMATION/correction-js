7a458ae7dee53db6ff2390566cd1fe05113fe006
MedicArwen <thierry-bru@laposte.net>
2022-12-15 10:18:20 +0100

    correction série sur les fonctions

---
+++ s3/serie-1/exo-03.js
@@ -0,0 +1,6 @@
+// saisir un nombre d'étoile qui est bien un nombre entre 1 et 10 inclus
+let nombreEtoileDemande = saisirNombreEtoiles(1,10);
+for(let i=1;i<=nombreEtoileDemande;i++)
+{
+    console.log(retournerTexteEtoiles(i));
+}
