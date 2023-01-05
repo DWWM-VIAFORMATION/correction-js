c40128362061c5b31044d120b6807a21ed05032d
MedicArwen <thierry-bru@laposte.net>
2023-01-05 10:43:40 +0100

    correction fil du code

--- SEQ2-DOM-BOM/aufilducode/utilisation-dom-bom.js
+++ SEQ2-DOM-BOM/aufilducode/utilisation-dom-bom.js
@@ -16,7 +16,7 @@ console.log(window.innerWidth);
 console.log(window.outerHeight);
 console.log(window.outerWidth);
 //console.log(window.prompt("test"));
-//https://developer.mozilla.org/en-US/docs/Web/API/Window
+//https://developer.mozilla.org/en-US/docs/We0n0b/API/Window
 // événements
 window.addEventListener('resize',()=>{console.log('on change la taille!');})
 window.addEventListener('copy',()=>{console.log('Sale piwate! t a copié vilain!');})
@@ -35,4 +35,30 @@ console.log(navigator.platform);
 console.log(navigator.mediaDevices);
 // des actions: methodes
 // sos en morse
-navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100])
+//navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100])
+function coordonnees(pos) {
+    let crd = pos.coords;
+  console.log(pos);
+    let latitude = crd.latitude;
+    let longitude = crd.longitude;
+    
+    document.getElementById('p1').innerHTML= 'Latitude : ' + latitude.toFixed(2);
+    document.getElementById('p2').innerHTML= 'Longitude : ' + longitude.toFixed(2);
+}
+navigator.geolocation.getCurrentPosition(coordonnees);
+//document.querySelector('#back').addEventListener('click',()=>{console.log('bac');history.back()})
+document.querySelector('#back').addEventListener('click',()=>{console.log('bac');history.go(-3)})
+document.querySelector('#forward').addEventListener('click',()=>{console.log('forward');history.forward()})
+/**LOCATION */
+console.log(location.pathname);
+console.log(location.port);
+console.log(location.host);
+console.log(location.hostname);
+document.querySelector('#reload').addEventListener('click',()=>{console.log('reload');location.reload()})
+console.log(location.protocol);
+/** SCREEEN */
+document.querySelector('#p4').innerHTML =
+    'Dimensions totales de l\'écran : ' + screen.width + ' x ' + screen.height +
+    '<br>Surface disponible : ' + screen.availWidth + ' x ' + screen.availHeight +
+    '<br>Palette de couleur : ' + screen.colorDepth +
+    '<br>Résolution : ' + screen.pixelDepth;
